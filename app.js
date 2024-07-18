494a21116b5c6f93ed3fb7496e02c0ee80683245
rybarski <x23311801@student.ncirl.ie>
2024-07-18 19:43:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-const express = require('express');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const port = process.env.PORT || 8444;
+var indexRouter = require('./routes/index');
 
-// Example route
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+var app = express();
+var port = process.env.PORT || 8443;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// Additional routes can be added here
-app.get('/about', (req, res) => {
-    res.send('About Us');
-});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Catch-all route for 404 errors
-app.use((req, res, next) => {
-    res.status(404).send('Not Found');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
